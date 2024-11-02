c97b8d34b3926e75db74b0085f617f4f2170db6c
Gustavo Ornaghi Antunes <gustavoornaghiantunes@gmail.com>
2024-11-02 12:33:02 -0300

    improve alter page transition

--- assets/js/dynamic_pages.js
+++ assets/js/dynamic_pages.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
     $("a").on("click", (e) => {
-        e.preventDefault();
         url = e.currentTarget.href
+        if (url.includes("#")) return
+
+        e.preventDefault();
         content = $("#content")
-            $.get(url, (data) => {
-                content.css("opacity", 0)
-                setTimeout(() => {
-                    content.html(data)
-                    content.css("opacity", 1)
-                    history.pushState(null, null, url)
-                }, 500);
-            }).fail(() => {
-                alert("Erro ao carregar a pagina")
-            })
+        $.get(url, (data) => {
+            content.css("opacity", 0)
+            setTimeout(() => {
+                content.html(data)
+                content.css("opacity", 1)
+                history.pushState(null, null, url)
+            }, 250);
+        }).fail(() => {
+            alert("Erro ao carregar a pagina")
+        })
     })
 
     window.onpopstate = function() {
         const url = location.pathname.substring(1);
+        if (location.href.includes("#")) return
         $.get(url, function(data) {
             $('#conteudo').html(data).fadeIn(300);
         }).fail(function() {
